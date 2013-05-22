542560705f448075dd2f5c70e57fd0dba7505d77
Christophe VERGNE <christophe.vergne@gmail.com>
2013-05-22 13:09:14 +0200

    Poids des sous-titres - close #21
    + nom/poids barré si nul

--- api/freebox_files.php
+++ api/freebox_files.php
@@ -32,27 +32,42 @@
         }
         ksort($raw_files);
         foreach ($raw_files as $file) {
+            $sub_state_class = '';
+            $file_state_class = '';
             if (isset($file['file'], $file['sub'])) {
                 $size = convertFileSize($file['file']['size'], false);
+                if ($size['size'] == 0) {
+                    $file_state_class = ' class="unfinished"';
+                }
+                $subsize = convertFileSize($file['sub']['size'], false);
+                if ($subsize['size'] == 0) {
+                    $sub_state_class = ' unfinished';
+                }
                 $final_files[utf8_decode($file['file']['name'])] = '<tr>
-                        <td class="table-fs-filename"><code>' . utf8_decode($file['file']['name']) . '</code><br /><span class="text-muted">+</span>&nbsp;<code class="alt">' . utf8_decode($file['sub']['name']) . '</code></td>
-                        <td class="table-fs-size">' . $size['size'] . '&nbsp;' . ucfirst($size['unit']) . '</td>
+                        <td class="table-fs-filename"><code' . $file_state_class . '>' . utf8_decode($file['file']['name']) . '</code><br /><span class="text-muted">+</span>&nbsp;<code class="alt' . $sub_state_class . '">' . utf8_decode($file['sub']['name']) . '</code></td>
+                        <td class="table-fs-size"><span' . $file_state_class . '>' . $size['size'] . '&nbsp;' . ucfirst($size['unit']) . '</span><br/><small class="text-muted' . $sub_state_class . '">' . $subsize['size'] . '&nbsp;' . ucfirst($subsize['unit']) . '</small></td>
                         <td class="table-fs-remove"><a data-path="' . $dl_folder . '/' . utf8_decode($file['file']['name']) . '"><i class="glyphicon glyphicon-trash"></i></a></td>
                     </tr>';
             }
             else if (isset($file['sub'])) {
                 $size = convertFileSize($file['sub']['size'], false);
+                if ($size['size'] == 0) {
+                    $sub_state_class = ' unfinished';
+                }
                 $final_files[utf8_decode($file['sub']['name'])] = '<tr>
-                        <td class="table-fs-filename"><code class="alt">' . utf8_decode($file['sub']['name']) . '</code></td>
-                        <td class="table-fs-size">' . $size['size'] . '&nbsp;' . ucfirst($size['unit']) . '</td>
+                        <td class="table-fs-filename"><code class="alt' . $sub_state_class . '">' . utf8_decode($file['sub']['name']) . '</code></td>
+                        <td class="table-fs-size"><small class="text-muted' . $sub_state_class . '">' . $size['size'] . '&nbsp;' . ucfirst($size['unit']) . '</small></td>
                         <td class="table-fs-remove"><a data-path="' . $dl_folder . '/' . utf8_decode($file['sub']['name']) . '"><i class="glyphicon glyphicon-trash"></i></a></td>
                     </tr>';
             }
             else if (isset($file['file'])) {
                 $size = convertFileSize($file['file']['size'], false);
+                if ($size['size'] == 0) {
+                    $file_state_class = ' class="unfinished"';
+                }
                 $final_files[utf8_decode($file['file']['name'])] = '<tr>
-                        <td class="table-fs-filename"><code>' . utf8_decode($file['file']['name']) . '</code></td>
-                        <td class="table-fs-size">' . $size['size'] . '&nbsp;' . ucfirst($size['unit']) . '</td>
+                        <td class="table-fs-filename"><code' . $file_state_class . '>' . utf8_decode($file['file']['name']) . '</code></td>
+                        <td class="table-fs-size"><span' . $file_state_class . '>' . $size['size'] . '&nbsp;' . ucfirst($size['unit']) . '</span></td>
                         <td class="table-fs-remove"><a data-path="' . $dl_folder . '/' . utf8_decode($file['file']['name']) . '"><i class="glyphicon glyphicon-trash"></i></a></td>
                     </tr>';
             }
